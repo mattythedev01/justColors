ee96f59c30abb399f4034fafa3126e49e8687ebc
mattythedev01 <brennanhiltonrose0103@gmail.com>
2024-10-15 13:57:11 -0400

    Updated

--- index.js
+++ index.js
@@ -19,121 +19,28 @@ const emojiDecorations = require("./colors/emojiDecor");
 const newEffects = require("./colors/newEffects");
 
 const justColors = {
-  // Regular colors
-  red: colorFunctions.red,
-  green: colorFunctions.green,
-  blue: colorFunctions.blue,
-  yellow: colorFunctions.yellow,
-  magenta: colorFunctions.magenta,
-  cyan: colorFunctions.cyan,
-  white: colorFunctions.white,
-  gray: colorFunctions.gray,
-  black: colorFunctions.black,
-  orange: colorFunctions.orange,
-  purple: colorFunctions.purple,
-  pink: colorFunctions.pink,
-  brown: colorFunctions.brown,
-  navy: colorFunctions.navy,
-  teal: colorFunctions.teal,
-  maroon: colorFunctions.maroon,
-  olive: colorFunctions.olive,
-  lime: colorFunctions.lime,
-  coral: colorFunctions.coral,
-  salmon: colorFunctions.salmon,
-  gold: colorFunctions.gold,
-  indigo: colorFunctions.indigo,
-  turquoise: colorFunctions.turquoise,
-  violet: colorFunctions.violet,
-  plum: colorFunctions.plum,
-  khaki: colorFunctions.khaki,
-  crimson: colorFunctions.crimson,
-  orchid: colorFunctions.orchid,
-  sienna: colorFunctions.sienna,
-  tan: colorFunctions.tan,
-  chocolate: colorFunctions.chocolate,
-  peru: colorFunctions.peru,
-  indianRed: colorFunctions.indianRed,
-  darkGreen: colorFunctions.darkGreen,
-  darkBlue: colorFunctions.darkBlue,
-  darkRed: colorFunctions.darkRed,
-  darkCyan: colorFunctions.darkCyan,
-  darkMagenta: colorFunctions.darkMagenta,
-  darkOrange: colorFunctions.darkOrange,
-  darkViolet: colorFunctions.darkViolet,
-  darkGray: colorFunctions.darkGray,
-  lightGray: colorFunctions.lightGray,
-  lightGreen: colorFunctions.lightGreen,
-  lightBlue: colorFunctions.lightBlue,
-  lightPink: colorFunctions.lightPink,
-  lightSalmon: colorFunctions.lightSalmon,
-  lightCoral: colorFunctions.lightCoral,
-  lightSkyBlue: colorFunctions.lightSkyBlue,
-
-  // Bright colors
-  brightRed: colorFunctions.brightRed,
-  brightGreen: colorFunctions.brightGreen,
-  brightBlue: colorFunctions.brightBlue,
-  brightYellow: colorFunctions.brightYellow,
-  brightMagenta: colorFunctions.brightMagenta,
-  brightCyan: colorFunctions.brightCyan,
-  brightWhite: colorFunctions.brightWhite,
-
-  // Background colors
-  bgRed: colorFunctions.bgRed,
-  bgGreen: colorFunctions.bgGreen,
-  bgBlue: colorFunctions.bgBlue,
-  bgYellow: colorFunctions.bgYellow,
-  bgMagenta: colorFunctions.bgMagenta,
-  bgCyan: colorFunctions.bgCyan,
-  bgWhite: colorFunctions.bgWhite,
-  bgBlack: colorFunctions.bgBlack,
-
-  // Bright background colors
-  bgBrightRed: colorFunctions.bgBrightRed,
-  bgBrightGreen: colorFunctions.bgBrightGreen,
-  bgBrightBlue: colorFunctions.bgBrightBlue,
-  bgBrightYellow: colorFunctions.bgBrightYellow,
-  bgBrightMagenta: colorFunctions.bgBrightMagenta,
-  bgBrightCyan: colorFunctions.bgBrightCyan,
-  bgBrightWhite: colorFunctions.bgBrightWhite,
+  // Regular colors and background colors
+  ...colorFunctions,
 
   // Styles
   ...styleFunctions,
-
-  // Special effects
-  rainbow: specialEffects.rainbow,
-  zebra: specialEffects.zebra,
-  america: specialEffects.america,
-  trap: specialEffects.trap,
-  random: specialEffects.random,
-
-  // Pastel colors
-  pastelRed: pastelColors.pastelRed,
-  pastelGreen: pastelColors.pastelGreen,
-  pastelBlue: pastelColors.pastelBlue,
-  pastelYellow: pastelColors.pastelYellow,
-  pastelMagenta: pastelColors.pastelMagenta,
-  pastelCyan: pastelColors.pastelCyan,
-
-  // Additional colors
-  ...additionalColors,
-
-  // Additional background colors
-  ...additionalBackgroundColors,
-
-  // Additional styles
   ...additionalStyles,
 
+  // Special effects
+  ...specialEffects,
+
+  // Pastel colors
+  ...pastelColors,
+
+  // Additional colors and background colors
+  ...additionalColors,
+  ...additionalBackgroundColors,
+
   // Gradient effects
-  sunsetGradient: gradients.sunsetGradient,
-  oceanGradient: gradients.oceanGradient,
+  ...gradients,
 
   // Semantic colors
   ...semanticColors,
-
-  // Additional gradient effects
-  forestGradient: gradients.forestGradient,
-  lavenderGradient: gradients.lavenderGradient,
 
   // Neon colors
   ...neonColors,
@@ -144,6 +51,12 @@ const justColors = {
   // Color combinations
   ...colorCombinations,
 
+  // Emoji decorations
+  ...emojiDecorations,
+
+  // New effects
+  ...newEffects,
+
   // Text decorations
   frame: (text) => {
     const lines = text.split("\n");
@@ -153,12 +66,6 @@ const justColors = {
     const framedLines = lines.map((line) => "║ " + line.padEnd(width) + " ║");
     return [top, ...framedLines, bottom].join("\n");
   },
-
-  // Emoji decorations
-  ...emojiDecorations,
-
-  // New effects
-  ...newEffects,
 };
 
 module.exports = justColors;
